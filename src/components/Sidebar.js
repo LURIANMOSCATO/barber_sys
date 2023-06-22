d1e715fe125d6adc7e7c9b216f31b054684f7d00
Lurian Vargas <120066573+LURIANMOSCATO@users.noreply.github.com>
2023-06-22 16:03:18 -0300

    Update Sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -26,7 +26,7 @@ const Sidebar = ({children}) =>  {
             <div className="top_section">
                 <h1 style={{display: isOpen ? "block" : "none"}}  className="logo">logo</h1>
                 <div style={{marginLeft: isOpen ? "50px" : "0px"}}  className="bars">
-                    <div onClick={toggle}/>Menu</div>
+                    
                 </div>
             </div>
             <div>
